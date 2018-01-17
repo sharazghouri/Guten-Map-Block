9a8b55ac6ceb46afa09846e8ac38734d1eb0925a
abc235896 <bc140401486@vu.edu.pk>
2018-01-17 11:57:35 +0500

    inintal repo commint map blocks

---
+++ README.md
@@ -0,0 +1,6 @@
+# Guten-Starter-Kit
+Starter kit for Gutenburd dev 
+1. npm install
+2. npm run dev
+3. npm run build 
+<br> Sharaz ghouri
---
+++ block.js
@@ -0,0 +1,12 @@
+
+import css from  './style.css';
+const { __ } = wp.i18n;
+const {
+    registerBlockType,
+    Editable } = wp.blocks;
+
+export default registerBlockType( 'wpbirgade/map',{
+
+
+
+});
